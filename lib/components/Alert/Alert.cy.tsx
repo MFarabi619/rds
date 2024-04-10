3491ff7a634996916cfde9714c9f9dee2956bb46
Ish <ishdeep.711@gmail.com>
2024-04-10 15:56:15 -0400

    change: adding the custom styles to cypress component testing

---
+++ lib/components/Alert/Alert.cy.tsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+// import { Alert } from './Alert'
+
+describe('<Alert />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    // cy.mount(<Alert title={'This is a success alert '} type={'success'} />)
+  })
+})
